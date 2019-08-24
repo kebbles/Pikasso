cf0ca1322408660946def469747e9d859abc2f20
Eric Liang <xxliang@edu.uwaterloo.ca>
2019-08-24 19:05:26 -0400

    added secondary parsing of user data

--- backend/app/http/api/repository/schemas/user_preference.py
+++ backend/app/http/api/repository/schemas/user_preference.py
@@ -1,8 +1,8 @@
 from mongoengine import *
 
 
-class UserPreferenceSchema(Schema):
-    id = IntField(unique=True, required=True)
+class UserPreferenceSchema(Document):
+    user_id = IntField(primary_key=True, required=True)
     full_name = StringField(max_length=100, required=True)
-    user_preferences = DictField(required=True)
+    user_preferences = DictField()
     meta = {'db_alias': 'user_pref_alias'}
--- backend/app/http/api/services/db_manager.py
+++ backend/app/http/api/services/db_manager.py
@@ -4,6 +4,7 @@ import boto3
 from ..repository.global_rep import GlobalRepository
 from ..repository.user_rep import UserRepository
 from ..repository.schemas.global_freq import GlobalFrequencySchema
+from ..repository.schemas.user_preference import UserPreferenceSchema
 from .preference_quantifier import PreferenceQuantifier
 
 
@@ -15,16 +16,18 @@ class DBManager(object):
         self.user_repo_client = user_repo_client
         self.pref_quantifier = pref_quantifier
     
-    def calculate_uniqueness(self, global_user_freq):
+    def calculate_uniqueness(self, user_freq):
         total_sum = 0
-        for (key, value) in global_user_freq.items():
+        for (key, value) in user_freq.items():
             total_sum += value
-        return {key: 1-value/total_sum for (key, value) in global_user_freq.items()}
+        user_uniqueness_freq = {key: 1-value/total_sum for (key, value) in user_freq.items()}
+        return {key: (1 if value == 0 else value) for (key, value) in user_uniqueness_freq.items()}
 
     def find_unique_preferences(self, image, user_id):
         preference_results = self.pref_quantifier.find_unique_preferences(image, user_id)
-        global_user_freq = {}
+        UserPreferenceSchema(user_id=123, full_name="Eric", user_preferences={"Electronics": 0.8}).save()
         GlobalFrequencySchema(object_name="Electronics", object_freq=5).save()
+        global_user_freq = {}
         for (key, value) in preference_results.items():
             table_item = GlobalFrequencySchema.objects(object_name=key)
             if table_item:
@@ -36,5 +39,15 @@ class DBManager(object):
         # for freq in GlobalFrequencySchema.objects:
         #     print(freq.object_name, freq.object_freq)
         user_uniqueness_freq = self.calculate_uniqueness(global_user_freq)
-
+        print(user_uniqueness_freq)
+        for user in UserPreferenceSchema.objects:
+            other_user_freq = {}
+            for (key, value) in user.user_preferences.items():
+                table_item = GlobalFrequencySchema.objects(object_name=key)
+                if table_item:
+                    other_user_freq[key] = table_item.first().object_freq
+            UserPreferenceSchema.objects(user_id=user.user_id)\
+                .update(set__user_preferences=self.calculate_uniqueness(other_user_freq))
+        
+        UserPreferenceSchema.objects.delete()
         GlobalFrequencySchema.objects.delete()
