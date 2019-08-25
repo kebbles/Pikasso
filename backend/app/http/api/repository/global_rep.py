10cff37b1914c68d8d16c5d1d94bdd76c94c2707
Eric Liang <xxliang@edu.uwaterloo.ca>
2019-08-25 09:18:11 -0400

    fixed

--- backend/app/http/api/repository/global_rep.py
+++ backend/app/http/api/repository/global_rep.py
@@ -1,5 +1,4 @@
 import os
-from mongoengine import connect
 from pymongo import MongoClient
 
 
@@ -8,8 +7,20 @@ class GlobalRepository(object):
     COLLECTION_NAME = "global_frequencies"
 
     def __init__(self):
-        mongo_url = os.environ.get('MONGO_URL')
-        connection = MongoClient(mongo_url).database
-        self.db = connection.global_frequencies
-        connect(alias="global_freq_alias", db=GlobalRepository.COLLECTION_NAME)
+        self.db = MongoClient("mongodb+srv://EricLiang:198726543@cluster0-cb1lw.mongodb.net/test?retryWrites=true&w=majority").global_frequencies
         print("Created Global Repository")
+    
+    def find_all(self, selector):
+        return self.db.global_frequencies.find(selector)
+ 
+    def find(self, selector):
+        return self.db.global_frequencies.find_one(selector)
+    
+    def create(self, freq_entry):
+        return self.db.global_frequencies.insert_one(freq_entry)
+
+    def update(self, selector, freq_entry):
+        return self.db.global_frequencies.update_one(selector, freq_entry).modified_count
+    
+    def delete(self, selector):
+        return self.db.global_frequencies.delete_many(selector).deleted_count
--- backend/app/http/api/repository/user_rep.py
+++ backend/app/http/api/repository/user_rep.py
@@ -8,8 +8,20 @@ class UserRepository(object):
     COLLECTION_NAME = "user_preferences"
 
     def __init__(self):
-        mongo_url = os.environ.get('MONGO_URL')
-        connection = MongoClient(mongo_url).database
-        self.db = connection.user_preferences
-        connect(alias="user_pref_alias", db=UserRepository.COLLECTION_NAME)
+        self.db = MongoClient("mongodb+srv://EricLiang:198726543@cluster0-cb1lw.mongodb.net/test?retryWrites=true&w=majority").user_preferences
         print("Created User Repository")
+
+    def find_all(self, selector):
+        return self.db.user_preferences.find(selector)
+ 
+    def find(self, selector):
+        return self.db.user_preferences.find_one(selector)
+    
+    def create(self, freq_entry):
+        return self.db.user_preferences.insert_one(freq_entry)
+
+    def update(self, selector, user_entry):
+        return self.db.user_preferences.update_one(selector, user_entry).modified_count
+    
+    def delete(self, selector):
+        return self.db.user_preferences.delete_many(selector).deleted_count
